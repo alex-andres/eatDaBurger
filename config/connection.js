09754fadc736044ef706ab04e2be67dc6bb50477
c-andres <charlesalexanderandres@gmail.com>
2017-11-30 17:14:11 -0800

    heroku fix 2

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 const mysql = require("mysql");
 
+let connection;
 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-}else{
-
-  const connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  database: "burger_db"
-});
+}
+else{
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    database: "burger_db"
+  });
+}
 
 connection.connect((err) => {
   if (err) {
@@ -19,5 +21,5 @@ connection.connect((err) => {
   }
   console.log("connected as id " + connection.threadId);
 });
-}
+
 module.exports = connection;
