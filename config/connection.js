fbcf0aeb7b02563f0d64f50885007d8dea1a9a7b
c-andres <charlesalexanderandres@gmail.com>
2017-11-30 17:11:40 -0800

    heroku fix 2

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-}else{}
+}else{
 
   const connection = mysql.createConnection({
   port: 3306,
